06b21598a8e7932a1bfbf52a7eb9490a05dd0342
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-10-24 14:16:54 -0200

    Update rspec to 3.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,24 @@ require 'rspec'
 require 'pry'
 require 'boletosimples'
 
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+  config.order = :random
+  Kernel.srand config.seed
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+end
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }

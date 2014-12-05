d4dda416e661ad1f399a971cba33677d84ab941f
Kivanio Barbosa <kivanio@gmail.com>
2014-12-05 17:05:15 -0300

    add parcelled example

--- example/access_token_example.rb
+++ example/access_token_example.rb
@@ -32,6 +32,29 @@ client = BoletoSimples::Client.new('access_token', user_agent: 'Meu e-Commerce (
 
 # pp client.bank_billet 3
 
+# Boleto normal
+# pp client.create_bank_billet(
+#   {
+#     amount: 41.01,
+#     customer_address: 'Rua quinhentos',
+#     customer_address_complement: 'Sala 4',
+#     customer_address_number: '111',
+#     customer_city_name: 'Rio de Janeiro',
+#     customer_cnpj_cpf: '012.345.678-90',
+#     customer_email: 'cliente@bom.com',
+#     customer_neighborhood: 'Sao Francisco',
+#     customer_person_name: 'Joao da Silva',
+#     customer_person_type: 'individual',
+#     customer_phone_number: '2112123434',
+#     customer_state: 'RJ',
+#     customer_zipcode: '12312-123',
+#     description: 'Despesas do contrato 0012',
+#     expire_at: '2014-01-01',
+#     notification_url: 'http://example.com.br/notify'
+#   }
+# )
+
+# Boleto parcelado
 # pp client.create_bank_billet(
 #   {
 #     amount: 41.01,
@@ -50,5 +73,7 @@ client = BoletoSimples::Client.new('access_token', user_agent: 'Meu e-Commerce (
 #     description: 'Despesas do contrato 0012',
 #     expire_at: '2014-01-01',
 #     notification_url: 'http://example.com.br/notify',
+#     parcels: 3
 #   }
 # )
+
--- example/oauth_example.rb
+++ example/oauth_example.rb
@@ -80,6 +80,29 @@ client = BoletoSimples::OAuthClient.new('app_id',
 
 # pp client.bank_billet 3
 
+# Boleto normal
+# pp client.create_bank_billet(
+#   {
+#     amount: 41.01,
+#     customer_address: 'Rua quinhentos',
+#     customer_address_complement: 'Sala 4',
+#     customer_address_number: '111',
+#     customer_city_name: 'Rio de Janeiro',
+#     customer_cnpj_cpf: '012.345.678-90',
+#     customer_email: 'cliente@bom.com',
+#     customer_neighborhood: 'Sao Francisco',
+#     customer_person_name: 'Joao da Silva',
+#     customer_person_type: 'individual',
+#     customer_phone_number: '2112123434',
+#     customer_state: 'RJ',
+#     customer_zipcode: '12312-123',
+#     description: 'Despesas do contrato 0012',
+#     expire_at: '2014-01-01',
+#     notification_url: 'http://example.com.br/notify'
+#   }
+# )
+
+# Boleto parcelado
 # pp client.create_bank_billet(
 #   {
 #     amount: 41.01,
@@ -98,5 +121,6 @@ client = BoletoSimples::OAuthClient.new('app_id',
 #     description: 'Despesas do contrato 0012',
 #     expire_at: '2014-01-01',
 #     notification_url: 'http://example.com.br/notify',
+#     parcels: 3
 #   }
 # )

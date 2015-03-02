0eb8f71c03eebc684b92638ffbc027a65e88e617
Rafael Lima <contato@rafael.adm.br>
2015-03-02 13:07:57 -0300

    ignore host of codeclimate on vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,6 +4,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.ignore_hosts 'codeclimate.com'
   c.before_record do |i|
     i.response.body.force_encoding('UTF-8')
   end

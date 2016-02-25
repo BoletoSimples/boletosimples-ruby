46c8ff174278ff5afd72538c0c088f979b0f202e
Kivanio Barbosa <kivanio@gmail.com>
2016-02-25 16:11:33 -0400

    add pay_off endpoint

--- lib/boletosimples/resources/discharge.rb
+++ lib/boletosimples/resources/discharge.rb
@@ -1,4 +1,14 @@
 module BoletoSimples
   class Discharge < BaseModel
+    
+    def pay_off
+      self.class.request(:_method => :put, :_path => self.class.build_request_path('discharges/:id/pay_off', {self.class.primary_key => id})) do |parsed_data, response|
+        assign_attributes(self.class.parse(parsed_data[:data])) if parsed_data[:data].any?
+        @metadata = parsed_data[:metadata]
+        @response_errors = parsed_data[:errors]
+        @response = response
+      end
+      return @response.success?
+    end
   end
 end

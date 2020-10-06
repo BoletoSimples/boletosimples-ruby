ff27d38713f3b0e7cc0a6cc9f6a9573ab0195dd8
Kivanio Barbosa <kivanio@gmail.com>
2020-10-06 09:19:22 -0400

    Test cutom method from Her

--- lib/boletosimples/resources/bank_billet.rb
+++ lib/boletosimples/resources/bank_billet.rb
@@ -2,18 +2,10 @@
 
 module BoletoSimples
   class BankBillet < BaseModel
+    custom_put :pay
+
     def cancel
       self.class.request(_method: :put, _path: self.class.build_request_path('bank_billets/:id/cancel', { self.class.primary_key => id })) do |parsed_data, response|
-        assign_attributes(self.class.parse(parsed_data[:data])) if parsed_data[:data].any?
-        @metadata = parsed_data[:metadata]
-        @response_errors = parsed_data[:errors]
-        @response = response
-      end
-      @response.success?
-    end
-
-    def pay
-      self.class.request(_method: :put, _path: self.class.build_request_path('bank_billets/:id/pay', { self.class.primary_key => id })) do |parsed_data, response|
         assign_attributes(self.class.parse(parsed_data[:data])) if parsed_data[:data].any?
         @metadata = parsed_data[:metadata]
         @response_errors = parsed_data[:errors]
